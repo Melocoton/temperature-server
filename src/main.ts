ef1326fdf3200898e9a77868b15c80895d6b761d
Oscar Muñoz Moreno <oscar@raspberrypi>
2024-02-06 19:13:48 +0100

    MQTT Subscribe

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,14 @@
-console.log('Hello World');
+const mqtt = require("mqtt");
+
+const mqttClient = mqtt.connect("mqtt://raspberrypi.lan:1883");
+
+mqttClient.on('connect', () => {
+    console.log('Connected');
+    mqttClient.subscribe(['casa/temperature'], () => {
+        console.log('Subscribed');
+    });
+});
+
+mqttClient.on('message', (topic, payload) => {
+    console.log('Message received:', `Topic:${topic}, Payload:${payload}`);
+});
